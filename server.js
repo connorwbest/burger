48045b6a4d75c823f4b58dba816d7e414a5a79ab
connorw <connorwbest95@gmail.com>
2018-08-18 10:02:01 -0400

    changed structure

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var PORT = process.env.PORT || 8080;
-
 var app = express();
-
-app.use(express.static("public"));
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(bodyParser.json());
-
 var exphbs = require('express-handlebars');
-
-app.engine("handlebars", exphbs({defaultLayout: 'main'}));
-app.set("view engine", "handlebars");
-
 var routes = require("./controllers/movies_controller.js");
 
+app.use(express.static("public"));
+app.use(routes);
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.engine("handlebars", exphbs({defaultLayout: 'main'}));
+app.set("view engine", "handlebars");
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
@@ -25,10 +18,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
-
-app.use(routes);
-
-
 
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
