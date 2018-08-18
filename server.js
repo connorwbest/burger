16d67596846b53df6e8f7f66abd4f3275c42199c
connorw <connorwbest95@gmail.com>
2018-08-18 08:58:38 -0400

    heroku fix

--- server.js
+++ server.js
@@ -18,7 +18,17 @@ app.set("view engine", "handlebars");
 
 var routes = require("./controllers/movies_controller.js");
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.use(routes);
+
+
 
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
